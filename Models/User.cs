1437ed312a761eb6ddf38703c2bc83cb33f11fc5
Yaroslav040 <121113577+Yaroslav040@users.noreply.github.com>
2025-05-22 12:00:31 +0300

    Add Models/User.cs 1747904431498

---
+++ Models/User.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace MilitaryOffenseAutomation.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Role { get; set; }  // "Admin" or "Serviceman"
+    }
+}
